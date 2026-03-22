3b40bc013f552f193280076ae6d9c325eb0e8812
Aniket Gupta <aniketgupta07122001@gmail.com>
2026-03-22 07:47:13 +0530

    nuances

--- modules/vpc.tf
+++ modules/vpc.tf
@@ -1,5 +1,7 @@
 resource "aws_vpc" "project1-VPC" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
   tags = {
     Name = "project1-VPC"
   }
